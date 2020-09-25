7662700bef268650c82dd8a9282ee29a8f79abed
ofry <tim4job@bmail.ru>
2020-09-25 10:59:30 +0300

    Add factory.

---
+++ module/Application/src/Factory/WebPageFactory.php
@@ -0,0 +1,37 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: ofryak
+     * Date: 05.08.18
+     * Time: 9:38
+     */
+
+    namespace Application\Factory;
+
+    use Application\Model\WebPage;
+    use Laminas\ServiceManager\Factory\FactoryInterface;
+    use Interop\Container\ContainerInterface;
+
+    /**
+     * Class WebPageFactory Фабрика, описывающая веб-ресурс
+     *
+     * @package Application\Factory
+     */
+    class WebPageFactory implements FactoryInterface
+    {
+        /**
+         * @param \Interop\Container\ContainerInterface $container
+         * @param string                                $requestedName
+         * @param array|null                            $options
+         *
+         * @return \Application\Model\WebPage
+         */
+        public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+        {
+            $logger = $container->get('MyLogger');
+            $config = $container->get('config');
+            $client_options = isset($config['client']['options']) ?
+                $config['client']['options'] : array();
+            return new WebPage($logger, $client_options);
+        }
+    }
